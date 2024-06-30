409a523773bdd55549d92761f8e3b8d4b39c99fb
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-06-30 22:52:48 +0200

    Migrate to Graddle & Auto publish to Hangar (#13) (#14)
    * Migrate to Gradle
    * Add workflows
    * Add fix-gradlew.bat and fix gradlew
    * Update Paper version

---
+++ build.gradle.kts
@@ -0,0 +1,103 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+import java.io.ByteArrayOutputStream
+
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
+}
+
+repositories {
+    mavenLocal()
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+    maven { url = uri("https://oss.sonatype.org/content/groups/public/") }
+    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://repo.maven.apache.org/maven2/") }
+}
+
+dependencies {
+    compileOnly(libs.io.papermc.paper.paper.api)
+    compileOnly(libs.com.github.milkbowl.vaultapi)
+    compileOnly(libs.net.luckperms.api)
+}
+
+group = "com.lyttledev"
+version = "2.3.0"
+description = "LyttleEssentials"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+tasks.named<ProcessResources>("processResources") {
+    filesMatching("plugin.yml") {
+        expand("projectVersion" to project.version)
+    }
+}
+
+// Helper methods
+fun executeGitCommand(vararg command: String): String {
+    val byteOut = ByteArrayOutputStream()
+    exec {
+        commandLine = listOf("git", *command)
+        standardOutput = byteOut
+    }
+    return byteOut.toString(Charsets.UTF_8.name()).trim()
+}
+
+fun latestCommitMessage(): String {
+    return executeGitCommand("log", "-1", "--pretty=%B")
+}
+
+// Add -SNAPSHOT to the version if the channel is not Release
+val versionString: String =  if (System.getenv("CHANNEL") == "Release") {
+    version.toString()
+} else {
+    if (System.getenv("GITHUB_RUN_NUMBER") != null) {
+        "${version}-SNAPSHOT+${System.getenv("GITHUB_RUN_NUMBER")}"
+    } else {
+        "$version-SNAPSHOT"
+    }
+}
+
+// Get the channel from the environment variable or default to Alpha
+val envChannel = System.getenv("CHANNEL") ?: "Alpha"
+
+// Get the latest commit message for the changelog
+val changelogContent: String = latestCommitMessage()
+
+// Log the version and channel
+println("Version: $versionString")
+println("Channel: $envChannel")
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(versionString)
+        channel.set(envChannel)
+        changelog.set(changelogContent)
+        id.set("LyttleEssentials")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+
+                // Get platform versions from gradle.properties file
+                val versions: List<String> = (property("paperVersion") as String)
+                    .split(",")
+                    .map { it.trim() }
+                platformVersions.set(versions)
+            }
+        }
+    }
+}
